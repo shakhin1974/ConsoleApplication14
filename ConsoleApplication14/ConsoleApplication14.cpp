5210f1c3d8a36e59caee0095961b8319864f7f1f
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-14 12:00:32 +0300

    ConsoleApplication14.cpp

--- ConsoleApplication14/ConsoleApplication14.cpp
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -4,18 +4,19 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-void Person ::Sur_name(string surname) 
-{ 	if (surname > "А" && surname < "Я")
-	{ sur_name = surname; 	}
+void Person ::Sur_name(string surname_) 
+{ 	if (surname_ > "А" && surname_ < "Я")
+	{ surname = surname_; 	}
 	else
-		sur_name = "#######"; }
+		surname_ = "#######"; }
 void Person::Name(string name_)
 { name = name_;
 }void Person::F_name(string f_name_)
 { f_name = f_name_; }
 void Person ::Age(int age_)
 {
-	if (age_ <= 101 && age_ >= 0) { age = age_; }
+	
+	if (age_ <= 101 && age_ >= 0) { age = age_ ; }
 	else { age = 0; }};
 void Person::Gend(char gend_)
 { if (gend_ == 'М' or gend_ == 'Ж')
@@ -32,11 +33,15 @@ void Person::CH_Arr(string* _arr)
 {
 	ch_arr = _arr;
 }
-void Person::Parents(string f_name, string f_surname) {
+void Person::Parents(string f_name, 
+	string f_surname, string m_name, string m_surname) 
+{
 	if (f_name > "А" && f_name < "Я" &&
-		f_surname> "А" && f_surname < "Я")
+		f_surname> "А" && f_surname < "Я" && m_name > "А" && m_name < "Я" &&
+		m_surname> "А" && m_surname < "Я")
 	{
-		parents = "Отец " + f_name + ' ' + f_surname;
+	parents = "\nОтец: " + f_name + ' ' + f_surname
+	+ "\nМама: " + m_name + ' ' + m_surname + "\n";
 	}
 	else
 		parents = "#########";
@@ -51,6 +56,7 @@ SetConsoleOutputCP(1251);
 	Ivan.F_name("Олегович");
 	Ivan.Age(37);
 	Ivan.Gend('М');
+	Ivan.Parents("Виктор ", "Смирнов", "Анна", "Смирнова");
  	Ivan.Phone("89888081218");
 	string ch[] = { "Лена","Иван","Николай", "Анна" };
 	Ivan.CH_Arr(ch);
