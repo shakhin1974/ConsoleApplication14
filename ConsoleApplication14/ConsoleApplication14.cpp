2cf63f6039a4f1e4b9fbd5cf278d9fe5b5ba5bc7
shakhin1974 <57936556+shakhin1974@users.noreply.github.com>
2024-03-14 10:26:09 +0300

    ConsoleApplication14.cpp

--- ConsoleApplication14/ConsoleApplication14.cpp
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -1,7 +1,8 @@
-﻿#include <iostream>
+#include <iostream>
 #include<windows.h>
 #include"Header.h"
 #include<string>
+#include<algorithm>
 using namespace std;
 void Person ::Sur_name(string surname) 
 { 	if (surname > "А" && surname < "Я")
@@ -17,13 +18,28 @@ void Person ::Age(int age_)
 	if (age_ <= 101 && age_ >= 0) { age = age_; }
 	else { age = 0; }};
 void Person::Gend(char gend_)
+{ if (gend_ == 'М' or gend_ == 'Ж')
+	{ gend = gend_; }
+	else
+		gend = '#'; }
+void Person::Phone(string phone_)
+{ 	if (phone_.length() == 11)
+	{ phone = phone_; }
+	else
+		phone = "#############";
+}
+void Person::CH_Arr(string* _arr)
 {
-	if (gend_ == 'М' or gend_ == 'Ж')
+	ch_arr = _arr;
+}
+void Person::Parents(string f_name, string f_surname) {
+	if (f_name > "А" && f_name < "Я" &&
+		f_surname> "А" && f_surname < "Я")
 	{
-		gend = gend_;
+		parents = "Отец " + f_name + ' ' + f_surname;
 	}
 	else
-		gend = '#';
+		parents = "#########";
 }
 int main()
 {SetConsoleCP(1251);
@@ -33,10 +49,12 @@ SetConsoleOutputCP(1251);
 	Ivan.Sur_name("Смирнов");
 	Ivan.Name("Иван");
 	Ivan.F_name("Олегович");
-	Ivan.Age(17);
+	Ivan.Age(37);
 	Ivan.Gend('М');
-	string arr[] = {"Илья", "Лена", "Николай", "Анна"};
-	Ivan.Arr( arr );
-	Ivan.Vivod();
+ 	Ivan.Phone("89888081218");
+	string ch[] = { "Лена","Иван","Николай", "Анна" };
+	Ivan.CH_Arr(ch);
+	int n = size(ch);
+	Ivan.Vivod(n);
 	
 }
